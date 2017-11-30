fdcd0d3afb7555b2917a469f7380bb99ab7fe61b
Marco Tulio Arantes <marcotuliio.aarantes@gmail.com>
2017-11-29 23:26:45 -0200

    v1.0.3 - CRUD Cadastro - Entities - Insert

---
+++ SaaS_App/SaaS_App/DAL/Tb_Empresa_DAO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using SaaS_App.DAL;
+using SaaS_App.Entidades;
+using System.Text;
+
+namespace SaaS_App.DAL
+{
+    public class Tb_Empresa_DAO
+    {
+        Conn.Conexao Db = new Conn.Conexao();
+
+        public Boolean Insert(Tb_Empresa Obj)
+        {
+            MySqlConnection Conexao = new MySqlConnection();
+            MySqlCommand Comando = new MySqlCommand();
+            StringBuilder Sql = new StringBuilder();
+
+            Sql.Append("INSERT INTO db_app.tb_empresa (vNomEmpresa, vNomResponsavel,vNumCnpjCpf, " +
+                                                        "vNumTelefoneComercial, vNumCelular, vCep," +
+                                                        "vEndereco, vCidade, vUf ) VALUES " +
+                                                        "(@vNomEmpresa, @vNomResponsavel, @vNumCnpjCpf," +
+                                                        "@vNumTelefoneComercial, @vNumCelular, @vCep, " + "" +
+                                                        "@vEndereco, @vCidade, @vUf )");
+
+            try
+            {
+                Conexao = Db.GetConexao();
+
+                Comando.Connection = Conexao;
+                Comando.CommandText = Sql.ToString();
+                Comando.Parameters.AddWithValue("@vNomEmpresa", Obj.vNomEmpresa);
+                Comando.Parameters.AddWithValue("@vNomResponsavel", Obj.vNomResponsavel);
+                Comando.Parameters.AddWithValue("@vNumCnpjCpf", Obj.vNumCnpjCpf);
+                Comando.Parameters.AddWithValue("@vNumTelefoneComercial", Obj.vNumTelefoneComercial);
+                Comando.Parameters.AddWithValue("@vNumCelular", Obj.vNumCelular);
+                Comando.Parameters.AddWithValue("@vCep", Obj.vCep);
+                Comando.Parameters.AddWithValue("@vEndereco", Obj.vEndereco);
+                Comando.Parameters.AddWithValue("@vCidade", Obj.vCidade);
+                Comando.Parameters.AddWithValue("@vUf", Obj.vUf);
+
+                Comando.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (Conexao.State == System.Data.ConnectionState.Open)
+                {
+                    Conexao.Close();
+                }
+            }
+        }
+    }
+}
---
+++ SaaS_App/SaaS_App/Entidades/Tb_Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaaS_App.Entidades
+{
+    public class Tb_Empresa
+    {
+        public int iCodEmpresa { get; set; }
+        public int iCodConta { get; set; }
+        public string vNomEmpresa { get; set; }
+        public string vNomResponsavel { get; set; }
+        public string vNumCnpjCpf { get; set; }
+        public string vNumTelefoneComercial { get; set; }
+        public string vNumCelular { get; set; }
+        public string vCep { get; set; }
+        public string vEndereco { get; set; }
+        public string vCidade { get; set; }
+        public string vUf { get; set; }
+
+
+    }
+}
